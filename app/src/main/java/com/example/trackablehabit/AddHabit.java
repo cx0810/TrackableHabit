4f55b24d2e5b29a1641374610fea7bc56d0b63de
cx0810 <chengxin0810@gmail.com>
2020-05-20 19:03:37 +0800

    added different pages (homepage, user manual, statistics, create)

---
+++ app/src/main/java/com/example/trackablehabit/AddHabit.java
@@ -0,0 +1,14 @@
+package com.example.trackablehabit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddHabit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_habit);
+    }
+}
