8f3c4411114440271990944bcab569cdb4fc7f5c
cx0810 <chengxin0810@gmail.com>
2020-07-20 18:49:21 +0800

    added userList for database table and fixed issues with navigating between pages

--- app/src/main/java/com/example/trackablehabit/LoginActivity.java
+++ app/src/main/java/com/example/trackablehabit/LoginActivity.java
@@ -9,17 +9,21 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
     EditText mTextUsername;
     EditText mTextPassword;
     Button mButtonLogin;
     TextView mRegister;
+    HabitDBHelper db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        db = new HabitDBHelper(this);
 
         mTextUsername = findViewById(R.id.login_username);
         mTextPassword = findViewById(R.id.login_password);
@@ -29,6 +33,19 @@ public class LoginActivity extends AppCompatActivity {
         mRegister.setOnClickListener(v -> {
             Intent startIntent = new Intent(getApplicationContext(), RegisterActivity.class);
             startActivity(startIntent);
+        });
+
+        mButtonLogin.setOnClickListener(v -> {
+            String user = mTextUsername.getText().toString().trim();
+            String password = mTextPassword.getText().toString().trim();
+            boolean res = db.checkUser(user, password);
+            if (res) {
+                Toast.makeText(LoginActivity.this, "Successfully Logged In", Toast.LENGTH_SHORT).show();
+                Intent homepage = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(homepage);
+            } else {
+                Toast.makeText(LoginActivity.this, "Login Error", Toast.LENGTH_SHORT).show();
+            }
         });
 
 
--- app/src/main/java/com/example/trackablehabit/RegisterActivity.java
+++ app/src/main/java/com/example/trackablehabit/RegisterActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
+    HabitDBHelper db;
     EditText mTextUsername;
     EditText mTextPassword;
     EditText mTextConfirmPassword;
@@ -20,6 +23,8 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        db = new HabitDBHelper(this);
+
         mTextUsername = findViewById(R.id.register_username);
         mTextPassword = findViewById(R.id.register_password);
         mTextConfirmPassword = findViewById(R.id.register_confirm_password);
@@ -29,6 +34,26 @@ public class RegisterActivity extends AppCompatActivity {
         mLogin.setOnClickListener(v -> {
             Intent startIntent = new Intent(getApplicationContext(), LoginActivity.class);
             startActivity(startIntent);
+        });
+
+        mButtonRegister.setOnClickListener(v -> {
+            String user = mTextUsername.getText().toString().trim();
+            String password = mTextPassword.getText().toString().trim();
+            String confirmPassword = mTextConfirmPassword.getText().toString().trim();
+
+            if (password.equals(confirmPassword)) {
+                long val = db.addUser(user, password);
+                if (val > 0) {
+                    Toast.makeText(RegisterActivity.this, "You have registered successfully", Toast.LENGTH_SHORT).show();
+                    Intent movetoLogin = new Intent(RegisterActivity.this, LoginActivity.class);
+                    startActivity(movetoLogin);
+                } else {
+                    Toast.makeText(RegisterActivity.this, "Registration Error", Toast.LENGTH_SHORT).show();
+                }
+            } else {
+                Toast.makeText(RegisterActivity.this, "Password does not match", Toast.LENGTH_SHORT).show();
+            }
+
         });
     }
 }
