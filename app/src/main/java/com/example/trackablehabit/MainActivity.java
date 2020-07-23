b8b50146a7170237b1403a507ae6461b38ff8ac2
cx0810 <chengxin0810@gmail.com>
2020-07-24 00:46:52 +0800

    added navigation drawer

--- app/src/main/java/com/example/trackablehabit/MainActivity.java
+++ app/src/main/java/com/example/trackablehabit/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.trackablehabit;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -13,6 +17,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.drm.DrmStore;
 import android.nfc.Tag;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -33,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
 
     public SQLiteDatabase habitDatabase;
     public HabitDBHelper habitDBHelper;
+    private DrawerLayout drawer;
 
     // arraylists
     private ArrayList<String> habit_name;
@@ -45,6 +51,15 @@ public class MainActivity extends AppCompatActivity {
 
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
         bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer,
+                toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
 
         habitDBHelper = new HabitDBHelper(this);
         habitDatabase = habitDBHelper.getReadableDatabase();
@@ -60,6 +75,17 @@ public class MainActivity extends AppCompatActivity {
 
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                 new HabitFragment()).commit();
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
     void storeDataInArrays() {
